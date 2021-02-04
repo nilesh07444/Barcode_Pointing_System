2c2844e6f8907de3a7bf9c98a3cffca503f6d860
Nilesh Prajapati <nilesh.prajapati.hs@gmail.com>
2021-02-04 23:36:09 +0530

    TermsCondition page added

---
+++ BarcodeSystem/BarcodeSystem/Areas/Admin/Controllers/TermsConditionController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BarcodeSystem.Areas.Admin.Controllers
+{
+    public class TermsConditionController : Controller
+    {        
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

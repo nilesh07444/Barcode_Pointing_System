02be177d9f634729e2903d7b1de2728a021c11e3
Nilesh Prajapati <nilesh.prajapati.hs@gmail.com>
2021-02-12 00:21:57 +0530

    abc

--- BarcodeSystem/BarcodeSystem/obj/Release/Package/PackageTmp/Content/assets/js/datatables/custom-basic.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('product-list').DataTable({
-        "pageLength": 100
-    });
-    // Basic table example
-    $('#basic-1').DataTable({
-        "pageLength": 100
-    });
-});

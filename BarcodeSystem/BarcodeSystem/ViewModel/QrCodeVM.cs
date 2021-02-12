94ec7d224272ec3c3b5aeb745b6d5389162dc6ea
Nilesh Prajapati <nilesh.prajapati.hs@gmail.com>
2021-02-12 23:59:34 +0530

    view & active/inactive qr code

--- BarcodeSystem/BarcodeSystem/ViewModel/QrCodeVM.cs
+++ BarcodeSystem/BarcodeSystem/ViewModel/QrCodeVM.cs
@@ -7,12 +7,11 @@ namespace BarcodeSystem
 {
     public class QrCodeVM
     {
-        public int QRCodeId { get; set; }
+        public long QRCodeId { get; set; }
         public string QRCode { get; set; }
-        public decimal Amount { get; set; }
+        public decimal? Amount { get; set; }
         public bool IsUsed { get; set; }
-        public int UsedBy { get; set; }
-        public string UsedByName { get; set; }
-
+        public long? UsedBy { get; set; }
+        public string UsedByName { get; set; } 
     }
 }
